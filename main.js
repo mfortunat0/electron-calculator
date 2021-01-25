b579ed2a104e084b0fb2fca1f81f3f766fae6b20
matheus <matheus.xmaz10@gmail.com>
2021-01-25 12:11:39 -0300

    Added electron in project

---
+++ main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 320,
+        height: 550,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    win.setResizable(false)
+    win.removeMenu()
+    win.loadFile('index.html')
+}
+app.whenReady().then(createWindow)
